69b8513391c5f8e605e11320ed8139b0b6607876
Кирилл <demidovcyrill@yandex.ru>
2023-04-06 22:04:13 +0300

    main

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from telegram.ext import CommandHandler
 import logging
+from random import choice
 from telegram.ext import Application, MessageHandler, filters
 BOT_TOKEN = '6048853518:AAFE1tEkAVFrJHw8YE8Rw3IYxuZmXo9fCyw'
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+echo_data = ['Простите, но я не понимаю что вы говорите...', 'Надо же, не знал что это есть в мире One Piece!',
+             'Если это есть в One Piece, то королём пиратов станет Усопп!',
+             'Это точно есть в One Piece? Пойду пересмотрю арку энис лобби...',
+             'Хммм... Возможно я упустил это в арке Алабаста, пойду пересмотрю',
+             'Я помнб что-то похожее в арке страны Вано, пойду пересмотрю...']
 
 
 async def echo(update, context):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text(choice(echo_data))
+    #update.message.text
 
 async def help(update, context):
     await update.message.reply_text("help")
